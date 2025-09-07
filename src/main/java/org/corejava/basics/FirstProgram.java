0638e931c644be2c1ad9114fb4f84b3bcec3fa7e
Pooja Gupta <pg190895@gmail.com>
2025-09-07 21:43:37 +0530

    FirstProgram Hello Example

---
+++ src/main/java/org/corejava/basics/FirstProgram.java
@@ -0,0 +1,13 @@
+package org.corejava.basics;
+
+public class FirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello lets start practice coding");
+    }
+
+    static{
+        System.out.println("inside static block");
+    }
+
+}
